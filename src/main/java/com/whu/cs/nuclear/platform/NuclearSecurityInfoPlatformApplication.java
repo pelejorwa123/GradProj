6f72ddd3d1ce5cdc6b041bbd678322055ebe2d19
pele <1461775766@qq.com>
2019-03-23 15:55:20 +0800

    create springboot project via maven

---
+++ src/main/java/com/whu/cs/nuclear/platform/NuclearSecurityInfoPlatformApplication.java
@@ -0,0 +1,13 @@
+package com.whu.cs.nuclear.platform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NuclearSecurityInfoPlatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NuclearSecurityInfoPlatformApplication.class, args);
+	}
+
+}
